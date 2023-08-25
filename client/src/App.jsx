774e84b548d91960e82e109daf7efc66b949847a
Andrew Pecyna <pecyna.andrew@gmail.com>
2023-08-25 11:57:10 -0400

    render translation in FE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,57 +1,15 @@
-import { styled } from "styled-components"
-import { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
 
-  const [userText, setUserText] = useState("")
-
-  const submitForm = async (e) => {
-    e.preventDefault()
-    
-    const translation = await generateTranslation()
-    console.log("returned from server: ", translation)
-  }
-
-  const generateTranslation = async () => {
-    const response = await fetch("http://localhost:3005/generate", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({ userText: userText })
-    })
-
-    const data = await response.json()
-    return data
-  }
-
   return (
-    <Container>
-      <h1>Translate text with Open AI</h1>
-      <form onSubmit={submitForm}>
-        <InputsBox>
-          <textarea
-          name="user-text"
-          placeholder="Enter text here"
-          onChange={(e) => setUserText(e.target.value)}
-          />
-          <br></br>
-          <input type="submit" value="Generate translation" />
-        </InputsBox>
-      </form>
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const InputsBox = styled.div`
-  display: flex;
-  flex-direction: column;
-`
 
 export default App
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+const LandingPage = () => {
+
+    const [userText, setUserText] = useState("")
+    const [aiTranslation, setAiTranslation] = useState("")
+
+    const submitForm = async (e) => {
+        e.preventDefault()
+    
+        const translation = await generateTranslation()
+        setAiTranslation(translation.response)
+        console.log("returned from server: ", translation.response)
+    }
+
+    const generateTranslation = async () => {
+        const response = await fetch("http://localhost:3005/generate", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({ userText: userText })
+        })
+
+    const data = await response.json()
+        return data
+    }
+
+    return (
+        <Container>
+            <h1>Translate text with Open AI</h1>
+            <form onSubmit={submitForm}>
+                <InputsBox>
+                    <div>
+                        <textarea
+                        name="user-text"
+                        placeholder="Type to translate"
+                        onChange={(e) => setUserText(e.target.value)}
+                        />
+                        <div>
+                            <p>The transaltion is: {aiTranslation}</p>
+                        </div>
+                    </div>
+                    <br></br>
+                    <input type="submit" value="Generate translation" />
+                </InputsBox>
+            </form>
+    </Container>
+    )
+}
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const InputsBox = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export default LandingPage;
